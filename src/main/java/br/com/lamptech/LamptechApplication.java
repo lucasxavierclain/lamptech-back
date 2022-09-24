1cf98de9a8329229c6ad6ef177bb66e71a761979
Lucas Xavier <lucasxavierclaindeabreu@gmail.com>
2022-09-24 13:38:25 -0300

    estrutura inicial

---
+++ src/main/java/br/com/lamptech/LamptechApplication.java
@@ -0,0 +1,13 @@
+package br.com.lamptech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LamptechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LamptechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lamptech/LamptechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lamptech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LamptechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
